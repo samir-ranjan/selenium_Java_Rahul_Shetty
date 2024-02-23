b31fad9bd4f749219d0cf3fa9f6e77366040f3d8
Samir Ranjan Parida <paridasamirranjan@gmail.com>
2024-02-23 23:02:47 +0530

    New Commit

---
+++ selenium_Udemy_R.S/src/test/java/pracProgs/Devide_A_No_By_Zero.java
@@ -0,0 +1,22 @@
+package pracProgs;
+
+public class Devide_A_No_By_Zero {
+	
+	/*
+	 * What will be the output when u divide a number by zero.
+	 * Number = integer, double, float
+	 */
+
+
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			System.out.println(2.2/0);
+			System.out.println(2/0.0);
+			System.out.println(12.33f/0);
+			System.out.println(2.2222d/0);
+
+		}
+
+
+}
