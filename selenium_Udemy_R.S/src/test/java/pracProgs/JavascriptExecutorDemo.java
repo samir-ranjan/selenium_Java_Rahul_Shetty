07507d38bbaf684cc9eb453f24de75a363f1bbe4
Samir Ranjan Parida <paridasamirranjan@gmail.com>
2024-02-26 22:28:00 +0530

    New Commit

--- selenium_Udemy_R.S/src/test/java/pracProgs/JavascriptExecutorDemo.java
+++ selenium_Udemy_R.S/src/test/java/pracProgs/JavascriptExecutorDemo.java
@@ -1,11 +1,18 @@
 package pracProgs;
 
-import java.time.Duration;
+import static org.testng.Assert.assertEquals;
 
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import dev.failsafe.internal.util.Assert;
 
 public class JavascriptExecutorDemo {
 
@@ -23,7 +30,23 @@ public class JavascriptExecutorDemo {
 		Thread.sleep(3000);
 		js.executeScript("document.querySelector(\".tableFixHead\").scrollTop=5000");
 		
-
+		List<WebElement> values = driver.findElements(By.cssSelector(".tableFixHead td:nth-child(4)"));
+		int sum = 0;
+		for(int i=0; i<values.size(); i++)
+		{
+			sum = sum + Integer.parseInt(values.get(i).getText());
+			
+		}
+		
+		System.out.println(sum);
+		
+		int Total = Integer.parseInt(driver.findElement(By.xpath("//div[@class='totalAmount']")).getText().split(":")[1].trim());
+		System.out.println(Total);
+		
+		assertEquals(sum, Total);
+		
+		
+		
 	}
 
 }
