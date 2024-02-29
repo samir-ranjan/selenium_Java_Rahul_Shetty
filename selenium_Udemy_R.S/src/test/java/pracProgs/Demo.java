c2578cc2fc1fe50c5cd3230cae1270d625f0f0c7
Samir Ranjan Parida <paridasamirranjan@gmail.com>
2024-02-29 23:16:15 +0530

    New commit

---
+++ selenium_Udemy_R.S/src/test/java/pracProgs/Demo.java
@@ -0,0 +1,28 @@
+package pracProgs;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Demo {
+
+	public static void main(String[] args) 
+	{
+		
+		
+		
+	
+		
+		System.setProperty("webdriver.chrome.driver", "E:\\Automation_Testing\\Selenium_Udemy\\Drivers\\chromedriver.exe");
+		ChromeOptions options = new ChromeOptions();
+		options.setAcceptInsecureCerts(true);
+		
+		WebDriver driver = new ChromeDriver(options);
+		driver.manage().window().maximize(); 
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://117.211.75.51:4443");
+	}
+
+}
